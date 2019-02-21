2c419fdb1e384392d6bc5d2b000781c37e5590f7
UmurerwaDivine <divinekagame@gmail.com>
2019-02-21 11:52:28 +0200

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,48 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from config import config_options
 
 bootstrap = Bootstrap()
-db = SQLAlchemy()
 
-#  Initializing application
-app = Flask(__name__,instance_relative_config = True)
 def create_app(config_name):
     app = Flask(__name__)
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
-bootstrap.init_app(app)
-db.init_app(app)
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-from app import views
-from app import error
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+
+    return app
+
+# from flask import Flask
+# from .config import DevConfig
+# from flask_bootstrap import Bootstrap
+# from flask_sqlalchemy import SQLAlchemy
+
+# bootstrap = Bootstrap()
+# db = SQLAlchemy()
+
+# #  Initializing application
+# app = Flask(__name__,instance_relative_config = True)
+# def create_app(config_name):
+#     app = Flask(__name__)
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile("config.py")
+
+# # Initializing Flask Extensions
+# bootstrap = Bootstrap(app)
+# bootstrap.init_app(app)
+# db.init_app(app)
+
+from .main import views
+from .main import error
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,9 @@
 from flask import render_template,request,redirect,url_for
-from app import app
-from .request import get_movies,get_movie,search_movie
-from app import views
-from .models import reviews
+from . import main
+from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm
-Review = reviews.Review
+from ..models import Review
+
 
 # Views
 # @app.route('/')
@@ -16,7 +15,7 @@ Review = reviews.Review
 
 #     message = 'Hello World'
 #     return render_template('index.html',message = message)
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -36,14 +35,14 @@ def index():
         return redirect(url_for('search',movie_name=search_movie))
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
-@app.route('/movies/<int:id>')
+@main.route('/movies/<int:id>')
 def movies(movie_id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
     return render_template('movie.html',id = movie_id)
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -54,7 +53,7 @@ def movie(id):
     reviews = Review.get_reviews(movie.id)
 
     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -64,7 +63,7 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
